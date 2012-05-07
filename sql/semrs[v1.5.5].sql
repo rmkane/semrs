29cae2dee38fdff10d92eaf8f82de0ed20072e3b
Ricky Orndorff <Richard.orndorff2012@gmail.com>
2012-05-07 01:09:17 -0400

    updated User's Guide

--- sql/semrs[v1.5.5].sql
+++ sql/semrs[v1.5.5].sql
@@ -26,6 +26,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `facility`
 --
 
+
 CREATE TABLE IF NOT EXISTS `facility` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL DEFAULT '',
